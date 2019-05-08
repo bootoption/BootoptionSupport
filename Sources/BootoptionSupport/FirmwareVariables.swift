dfde9f919db2d2fbbe9bbc86e439b55bfb9ae373
bootoption <48445527+bootoption@users.noreply.github.com>
2019-05-08 10:51:33 +0100

    Update

--- Sources/BootoptionSupport/FirmwareVariables.swift
+++ Sources/BootoptionSupport/FirmwareVariables.swift
@@ -10,101 +10,23 @@ import IOKit
 open class FirmwareVariables {
         private var options: io_registry_entry_t
         
+        private let appleBootGUID = "7C436110-AB2A-4BBB-A880-FE41995C9F82"
+        
+        private let efiGlobalGUID = "8BE4DF61-93CA-11D2-AA0D-00E098032B8C"
+        
         public static let `default` = FirmwareVariables()        
         
         private init() {
                 options = IORegistryEntryFromPath(kIOMasterPortDefault, "IODeviceTree:/options")
         }
         
-        open func setValue(forGlobalVariable name: String, value: Any) throws {
-                let key = "8BE4DF61-93CA-11D2-AA0D-00E098032B8C:" + name
-                try setCFProperty(value, forKey: key)
-                try syncNow(key)
-        }
-        
-        open func setValue(forVariable name: String, GUID: UUID? = nil, value: Any) throws {
-                var key = ""
-                if let uuidString = GUID?.uuidString {
-                        key = uuidString + ":"
-                }
-                key += name
-                try setCFProperty(value, forKey: key)
-                try syncNow(key)
-        }
-        
         public func createCFProperty(forKey key: String) -> CFTypeRef? {
                 return IORegistryEntryCreateCFProperty(options, key as CFString, kCFAllocatorDefault, 0)?.takeRetainedValue()
         }
         
-        public func dataValue(forGlobalVariable name: String) -> Data? {
-                let key = "8BE4DF61-93CA-11D2-AA0D-00E098032B8C:" + name
-                guard let data = createCFProperty(forKey: key) as? Data else {
-                        return nil
-                }
-                return data
-        }
-        
-        public func dataValue(forVariable name: String, GUID: UUID? = nil) -> Data? {
-                var key = ""
-                if let uuidString = GUID?.uuidString {
-                        key = uuidString + ":"
-                }
-                key += name
-                guard let data = createCFProperty(forKey: key) as? Data else {
-                        return nil
-                }
-                return data
-        }
-        
-        public func stringValue(forGlobalVariable name: String) -> String? {
-                let key = "8BE4DF61-93CA-11D2-AA0D-00E098032B8C:" + name
-                guard let string = createCFProperty(forKey: key) as? String else {
-                        return nil
-                }
-                return string
-
-        }
-        
-        public func stringValue(forVariable name: String, GUID: UUID? = nil) -> String? {
-                var key = ""
-                if let uuidString = GUID?.uuidString {
-                        key = uuidString + ":"
-                }
-                key += name
-                guard let string = createCFProperty(forKey: key) as? String else {
-                        return nil
-                }
-                return string
-        }
-        
-        public func delete(globalVariable name: String) throws {
-                let variable = "8BE4DF61-93CA-11D2-AA0D-00E098032B8C:" + name
-                let result = IORegistryEntrySetCFProperty(options, kIONVRAMDeletePropertyKey as CFString, variable as CFString)
-                switch result {
-                case KERN_SUCCESS:
-                        return
-                default:
-                        throw FirmwareVariablesError.delete(variable: name)
-                }
-        }
-        
-        public func delete(variable name: String, GUID: UUID? = nil) throws {
-                var variable = ""
-                if let uuidString = GUID?.uuidString {
-                        variable = uuidString + ":"
-                }
-                variable += name
-                let result = IORegistryEntrySetCFProperty(options, kIONVRAMDeletePropertyKey as CFString, variable as CFString)
-                switch result {
-                case KERN_SUCCESS:
-                        return
-                default:
-                        throw FirmwareVariablesError.delete(variable: name)
-                }
-        }
-        
         private func setCFProperty(_ value: Any, forKey name: String) throws {
                 var result = KERN_NOT_SUPPORTED
+                
                 switch value {
                 case is Bool:
                         if let bool = value as? Bool {
@@ -123,14 +45,58 @@ open class FirmwareVariables {
                                 result = IORegistryEntrySetCFProperty(options, name as CFString, data as CFData)
                         }
                 default:
-                        Debug.log("Failed to cast value for IORegistryEntrySetCFProperty", type: .error)
+                        Debug.log("Did not call IORegistryEntrySetCFProperty(:::) for unimplemented type %@", type: .error, argsList: String(describing: type(of: value)))
                         break
                 }
+                
                 switch result {
                 case KERN_SUCCESS:
                         return
                 default:
                         throw FirmwareVariablesError.set(variable: name)
+                }
+        }
+        
+        public func value<T>(forVariable name: String, GUID: UUID? = nil) -> T? {
+                var keyValue = ""
+                if let uuidString = GUID?.uuidString, uuidString != appleBootGUID {
+                        keyValue = uuidString + ":"
+                }
+                keyValue += name
+                return createCFProperty(forKey: keyValue) as? T
+        }
+        
+        public func dataValue(forVariable name: String, GUID: UUID? = nil) -> Data? {
+                var keyValue = ""
+                if let uuidString = GUID?.uuidString, uuidString != appleBootGUID {
+                        keyValue = uuidString + ":"
+                }
+                keyValue += name
+                return createCFProperty(forKey: keyValue) as? Data
+        }
+        
+        open func setValue(forVariable name: String, GUID: UUID? = nil, value: Any) throws {
+                var keyValue = ""
+                if let uuidString = GUID?.uuidString, uuidString != appleBootGUID {
+                        keyValue = uuidString + ":"
+                }
+                keyValue += name
+                try setCFProperty(value, forKey: keyValue)
+                try syncNow(keyValue)
+        }
+        
+        public func delete(variable name: String, GUID: UUID? = nil) throws {
+                var variable = ""
+                if let uuidString = GUID?.uuidString {
+                        variable = uuidString + ":"
+                }
+                variable += name
+                let result = IORegistryEntrySetCFProperty(options, kIONVRAMDeletePropertyKey as CFString, variable as CFString)
+                switch result {
+                case KERN_SUCCESS:
+                        return
+                default:
+                        throw FirmwareVariablesError.delete(variable: name)
                 }
         }
         
@@ -142,5 +108,24 @@ open class FirmwareVariables {
                 default:
                         throw FirmwareVariablesError.sync(variable: name)
                 }
+        }
+}
+
+public extension FirmwareVariables {
+        func value<T>(forGlobalVariable name: String) -> T? {
+                let result: T? = value(forVariable: name, GUID: UUID(uuidString: efiGlobalGUID))
+                return result
+        }
+        
+        func dataValue(forGlobalVariable name: String) -> Data? {
+                return dataValue(forVariable: name, GUID: UUID(uuidString: efiGlobalGUID))
+        }
+        
+        func setValue(forGlobalVariable name: String, value: Any) throws {
+                try setValue(forVariable: name, GUID: UUID(uuidString: efiGlobalGUID), value: value)
+        }
+        
+        func delete(globalVariable name: String) throws {
+                try delete(variable: name, GUID: UUID(uuidString: efiGlobalGUID))
         }
 }
